59a97fbd51c2d29a63e0dfd0f6d22b2b1f6b96c7
YuraBortnik <153739407+YuraBortnik@users.noreply.github.com>
2024-04-03 15:58:16 +0300

    Add files via upload

---
+++ Pz11/Pz11/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+
+namespace Pz11
+{
+    public abstract class Equation
+    {
+        public abstract double Eval();
+
+        public static implicit operator Equation(int source)
+        {
+            return new Const(source);
+        }
+
+    }
+
+    public class Add : Equation
+    {
+        private Equation left;
+        private Equation right;
+
+        public Add(Equation left, Equation right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public override double Eval()
+        {
+            return left.Eval() + right.Eval();
+        }
+    }
+
+    public class Subtract : Equation
+    {
+        private Equation left, right;
+
+        public Subtract(Equation left, Equation right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+        public override double Eval()
+        {
+            return right.Eval() - left.Eval();
+
+        }
+    }
+    public class Mult : Equation
+    {
+        private Equation left, right;
+
+        public Mult(Equation left, Equation right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+        public override double Eval()
+        {
+            return left.Eval() * right.Eval();
+
+        }
+
+    }
+    public class Div : Equation
+    {
+
+        private Equation left, right;
+        
+
+        
+        public Div(Equation left, Equation right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        
+        public override double Eval()
+        {
+            double denominator = right.Eval();
+            if (denominator == 0)
+            {
+                throw new DivideByZeroException("Ділення на нуль неможливе.");
+            }
+            return left.Eval() / denominator;
+        }
+    }
+
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Equation equation = new Add(new Const(5), new Mult(10, 4));
+            Console.WriteLine(equation.Eval());
+        }
+    }
+    public class Const : Equation
+
+    {
+        private int x;
+        public Const(int x)
+        {
+            this.x = x;
+        }
+
+        public override double Eval()
+        {
+            return x;
+        }
+
+
+    }
+
+}
---
+++ Pz11/Pz11/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Pz11")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pz11")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("66b68460-ed2f-4c2b-aacf-1427710ab253")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
